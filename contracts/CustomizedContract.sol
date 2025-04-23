bdfbe80a27f5c2a9ccbf3962bed2d19279138474
LesterCerioli <lesterlucasit@hotmail.com>
2025-04-23 12:05:40 -0700

    first commit

---
+++ contracts/CustomizedContract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract CustomizedContract {
+    string private message;
+
+    constructir(stribg memory _initialMessage) {
+        message = _initialMessage;
+    }
+
+    function setMessage(string memory _newMessage) public {
+        message = _newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
